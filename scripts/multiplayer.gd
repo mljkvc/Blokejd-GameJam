b15eff927bcef6fed53b30eed260c71ef7ae1c56
marko1684 <marko@math.com>
2025-02-08 18:02:34 +0100

    Added basic movement logic
    Implemented logic for checking player coquests of eachother but also
    for eating the treasure and respawning the players/trasure and
    points calculation

--- scripts/multiplayer.gd
+++ scripts/multiplayer.gd
@@ -4,6 +4,17 @@ var peer = ENetMultiplayerPeer.new()
 var board = []
 var current_turn = 1 # 1 is for player 1 and 2 is for player 2
 
+var player1_pos = Vector2(0, 5)
+var player2_pos = Vector2(7, 4)
+
+var player1_prev_pos = player1_pos
+var player2_prev_pos = player2_pos
+
+var player1_score = 0
+var player2_score = 0
+
+var treasure_pos = Vector2(randi() % 8, randi() % 8)
+
 func _ready():
 	_initialize_board()
 
@@ -43,3 +54,45 @@ func make_move(player_id, x, y, piece_data):
 func sync_game_state(new_board, turn):
 	board = new_board
 	current_turn = turn
+
+@rpc("any_peer")
+func make_move(player_id, x, y, piece_data, is_honest):
+	if multiplayer.get_remote_sender_id() != player_id:
+		return
+		
+	var prev_pos = player1_pos if player_id == 1 else player2_pos
+	var pos = Vector2(x, y)
+	
+	if player_id == 1:
+		player1_prev_pos = prev_pos
+		player1_pos = pos
+	else:
+		player2_prev_pos = prev_pos
+		player2_pos = pos
+	
+	# checking for the treasure conquest
+	
+	if pos == treasure_pos:
+		if is_honest:
+			if player_id == 1:
+				player1_score += 1
+			else:
+				player2_score += 1
+				
+			treasure_pos = Vector2(randi() % 8, randi() % 8)
+	
+	# checking for capturing of the enemy
+	
+	if player1_pos == player2_pos:
+		if player_id == 1:
+			player1_score += 1
+			player2_score -+ 1
+			player2_pos = Vector2(7, 4)
+		else:
+			player2_score += 1
+			player1_score -= 1
+			player1_pos = Vector2(0, 5)
+	
+	current_turn = 3 - current_turn
+	rpc("sync_game_state", board, player1_pos, player2_pos, current_turn, player1_score, player2_score, treasure_pos)
+		
