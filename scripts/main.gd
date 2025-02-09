561b9e44eed24e3a3534b20cfb33362d2d07f7e0
FilipAntonijevic <filipdantonijevic@gmail.com>
2025-02-09 06:44:09 +0100

    Updated songs

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,9 +6,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	main_menu.hide()
+	main_scene.hide()
 	main_menu.connect("join_lobby", Callable(self, "_on_join_lobby"))
-	play_song("res://doodle_lobby_song.mp3")
+	play_song("res://doodle_song.mp3")
 	
 func play_song(path_to_song: String):
 	var song = load(path_to_song)
@@ -28,8 +28,3 @@ func _on_join_lobby():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_audio_stream_player_2d_finished() -> void:
-	print('nemoguce')
-	audio_player.play()
