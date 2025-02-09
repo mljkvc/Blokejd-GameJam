0035fe9dd1cfe9c0e3d38179a520088641f82b0f
FilipAntonijevic <filipdantonijevic@gmail.com>
2025-02-09 14:15:10 +0100

    added underline animation

--- scripts/choose_a_piece_white.gd
+++ scripts/choose_a_piece_white.gd
@@ -2,16 +2,99 @@ class_name Choose_a_piece_white extends Node2D
 
 signal piece_chosen(piece_name: String)
 
+@onready var underline_1 = $underline1
+@onready var underline_2 = $underline2
+@onready var underline_3 = $underline3
+@onready var underline_4 = $underline4
+@onready var underline_5 = $underline5
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	underline_1.hide()
+	underline_2.hide()
+	underline_3.hide()
+	underline_4.hide()
+	underline_5.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
+func start_roulette_animation() -> void:
+	underline_1.show()
+	await get_tree().create_timer(0.1).timeout
+	underline_1.hide()
+	underline_2.show()
+	await get_tree().create_timer(0.1).timeout
+	underline_2.hide()
+	underline_3.show()
+	await get_tree().create_timer(0.1).timeout
+	underline_3.hide()
+	underline_4.show()
+	await get_tree().create_timer(0.1).timeout
+	underline_4.hide()
+	underline_5.show()
+	await get_tree().create_timer(0.1).timeout
+	underline_5.hide()
+	underline_1.show()
+	await get_tree().create_timer(0.2).timeout
+	underline_1.hide()
+	underline_2.show()
+	await get_tree().create_timer(0.2).timeout
+	underline_2.hide()
+	underline_3.show()
+	await get_tree().create_timer(0.2).timeout
+	underline_3.hide()
+	underline_4.show()
+	await get_tree().create_timer(0.2).timeout
+	underline_4.hide()
+	underline_5.show()
+	await get_tree().create_timer(0.2).timeout
+	underline_5.hide()
+	
+func underline_this_piece(piece_name: String):
+	start_roulette_animation()
+	if piece_name == "pawn":
+		underline_1.show()
+	if piece_name == "bishop":
+		underline_1.show()
+		await get_tree().create_timer(0.25).timeout
+		underline_1.hide()
+		underline_2.show()
+	if piece_name == "knight":
+		underline_1.show()
+		await get_tree().create_timer(0.25).timeout
+		underline_1.hide()
+		underline_2.show()
+		await get_tree().create_timer(0.3).timeout
+		underline_2.hide()
+		underline_3.show()
+	if piece_name == "rook":
+		underline_1.show()
+		await get_tree().create_timer(0.25).timeout
+		underline_1.hide()
+		underline_2.show()
+		await get_tree().create_timer(0.3).timeout
+		underline_2.hide()
+		underline_3.show()
+		await get_tree().create_timer(0.35).timeout
+		underline_3.hide()
+		underline_4.show()
+	if piece_name == "queen":
+		underline_1.show()
+		await get_tree().create_timer(0.25).timeout
+		underline_1.hide()
+		underline_2.show()
+		await get_tree().create_timer(0.3).timeout
+		underline_2.hide()
+		underline_3.show()
+		await get_tree().create_timer(0.35).timeout
+		underline_3.hide()
+		underline_4.show()
+		await get_tree().create_timer(0.4).timeout
+		underline_4.hide()
+		underline_5.show()
+		
 
 func _on_pawn_pressed() -> void:
 	emit_signal("piece_chosen", "pawn")
