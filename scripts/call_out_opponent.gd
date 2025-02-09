91b2bb0fd1882482b803686d6fa85ba50c68a283
mateja <stojanovic.mateja@gmail.com>
2025-02-09 18:14:02 +0100

    opet dugmaaaaaaaaaad

--- scripts/call_out_opponent.gd
+++ scripts/call_out_opponent.gd
@@ -23,21 +23,21 @@ func _on_scam_pressed() -> void:
 
 
 func _on_ok_mouse_entered() -> void:
-	ok_button.scale.x += 0.05
-	ok_button.scale.y += 0.05
+	ok_button.scale.x += 0.03
+	ok_button.scale.y += 0.03
 	ok_button.position.x -= 30
 
 func _on_ok_mouse_exited() -> void:
-	ok_button.scale.x -= 0.05
-	ok_button.scale.y -= 0.05
+	ok_button.scale.x -= 0.03
+	ok_button.scale.y -= 0.03
 	ok_button.position.x += 30
 
 func _on_scam_mouse_entered() -> void:
-	not_ok_button.scale.x += 0.05
-	not_ok_button.scale.y += 0.05
+	not_ok_button.scale.x += 0.03
+	not_ok_button.scale.y += 0.03
 	not_ok_button.position.x -= 30
 
 func _on_scam_mouse_exited() -> void:
-	not_ok_button.scale.x -= 0.05
-	not_ok_button.scale.y -= 0.05
+	not_ok_button.scale.x -= 0.03
+	not_ok_button.scale.y -= 0.03
 	not_ok_button.position.x += 30
