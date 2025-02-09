71684a4beca6b3a4d530d44ace051c67efe7ffcd
FilipAntonijevic <filipdantonijevic@gmail.com>
2025-02-09 13:10:55 +0100

    aaa

--- scripts/call_out_opponent.gd
+++ scripts/call_out_opponent.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@onready var ok_button = $ok
+@onready var not_ok_button = $scam
+
 signal ok_button_pressed()
 signal scam_button_pressed()
 
@@ -17,3 +20,24 @@ func _on_ok_pressed() -> void:
 
 func _on_scam_pressed() -> void:
 	emit_signal("scam_button_pressed")
+
+
+func _on_ok_mouse_entered() -> void:
+	ok_button.scale.x += 0.06
+	ok_button.scale.y += 0.06
+	ok_button.position.x -= 30
+
+func _on_ok_mouse_exited() -> void:
+	ok_button.scale.x -= 0.06
+	ok_button.scale.y -= 0.06
+	ok_button.position.x += 30
+
+func _on_scam_mouse_entered() -> void:
+	not_ok_button.scale.x += 0.06
+	not_ok_button.scale.y += 0.06
+	not_ok_button.position.x -= 30
+
+func _on_scam_mouse_exited() -> void:
+	not_ok_button.scale.x -= 0.06
+	not_ok_button.scale.y -= 0.06
+	not_ok_button.position.x += 30
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,8 @@
 class_name Main_menu extends Node2D
 
+@onready var create_lobby_button = $create_lobby
+@onready var join_lobby_button = $join_lobby
+
 signal create_lobby()
 signal join_lobby()
 signal set_your_pieces_to_white()
@@ -27,3 +30,25 @@ func _on_join_lobby_pressed() -> void:
 	print("CLIENT ID : PLS PLS PLS ", multiplayer.get_unique_id())
 	emit_signal("join_lobby")
 	emit_signal("set_your_pieces_to_black")
+
+
+func _on_create_lobby_mouse_entered() -> void:
+	create_lobby_button.scale.x += 0.08
+	create_lobby_button.scale.y += 0.08
+	create_lobby_button.position.x -= 40
+
+func _on_create_lobby_mouse_exited() -> void:
+	create_lobby_button.scale.x -= 0.08
+	create_lobby_button.scale.y -= 0.08
+	create_lobby_button.position.x += 40
+
+
+func _on_join_lobby_mouse_entered() -> void:
+	join_lobby_button.scale.x += 0.07
+	join_lobby_button.scale.y += 0.07
+	join_lobby_button.position.x -= 40
+
+func _on_join_lobby_mouse_exited() -> void:
+	join_lobby_button.scale.x -= 0.07
+	join_lobby_button.scale.y -= 0.07
+	join_lobby_button.position.x += 40
